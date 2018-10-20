15e6ff6ba2d2e4cd62f31983dbb462cb7ad6a806
Matej Minarik <mato.minarik@azet.sk>
2018-10-20 15:57:36 +0200

    Include Restaurant Name in the output

--- slack.js
+++ slack.js
@@ -2,8 +2,8 @@ const forkysParser = require("./parser/forkys").parse;
 const zlataLodParser = require("./parser/zlata-lod").parse;
 
 const parserMap = {
-  'forkys': forkysParser,
-  'lod': zlataLodParser,
+  forkys: { parser: forkysParser, restaurant: "Forkys" },
+  lod: { parser: zlataLodParser, restaurant: "Zlata Lod" }
 };
 
 function menuToText({ soups, mains }) {
@@ -11,18 +11,19 @@ function menuToText({ soups, mains }) {
 }
 
 function getParser(command) {
-  return parserMap[command];
+  return parserMap[command] || {};
 }
 
 async function commandHandler({ command }) {
-  const parser = getParser(command);
+  const { parser, restaurant } = getParser(command);
   if (!parser) {
     return { text: `Command '${command}' not supported.` };
   }
 
   try {
     const menu = await parser();
-    return { text: menuToText(menu) };
+    const menuText = menuToText(menu);
+    return { text: `:house: *${restaurant}*\n${menuText}` };
   } catch (error) {
     return { text: `Error: ${error}` };
   }
